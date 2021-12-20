a8b2fb2fe3e6538b1ccc8afc03e791a8b5181e2c
WilmerRS <estwilmerrodriguez@gmail.com>
2021-12-20 16:14:31 -0500

    add: api url in enviroments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
+  API_URL: 'https://969rgz78f9.execute-api.us-east-1.amazonaws.com/dev/api',
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  API_URL: 'https://969rgz78f9.execute-api.us-east-1.amazonaws.com/dev/api',
+  production: false,
 };
 
 /*
