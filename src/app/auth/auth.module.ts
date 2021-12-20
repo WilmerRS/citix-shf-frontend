ed3efe44e2520f09419288e4afbb04b818038f52
WilmerRS <estwilmerrodriguez@gmail.com>
2021-12-20 16:16:22 -0500

    add: auth module aggregate

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { AuthRoutingModule } from './auth-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AuthRoutingModule,
+    SharedModule
+  ]
+})
+export class AuthModule { }
